83672141b4ab5d326a44128892a556a44ecf3f41
Gifti Mulugeta <91586538+GiftiMulugeta@users.noreply.github.com>
2023-03-14 15:39:50 +0300

    Update fizzbuzz.py

--- Week1/fizzbuzz.py
+++ Week1/fizzbuzz.py
@@ -1,16 +1,29 @@
-class Solution(object):
+# class Solution(object):
     
+#     def fizzBuzz(self, n):
+#         empty_array=[]
+#         for i in range(1,n+1):
+#             if (i%3==0 and i%5 ==0):
+#                 empty_array.append("FizzBuzz")
+#             elif(i%3==0):
+#                 empty_array.append("Fizz")
+#             elif i%5==0:
+#                 empty_array.append("Buzz")
+#             else:
+#                 empty_array.append(str(i))
+#         return empty_array
+# fizzbuzz=Solution()
+# fizzbuzz.fizzBuzz(3)
+lass Solution(object):
     def fizzBuzz(self, n):
-        empty_array=[]
+        array=[]
         for i in range(1,n+1):
-            if (i%3==0 and i%5 ==0):
-                empty_array.append("FizzBuzz")
-            elif(i%3==0):
-                empty_array.append("Fizz")
-            elif i%5==0:
-                empty_array.append("Buzz")
+            if (i%3 == 0 and i%5 == 0):
+                array.append("FizzBuzz")
+            elif(i%5 == 0):
+                array.append("Buzz")
+            elif(i%3 == 0):
+                array.append("Fizz")
             else:
-                empty_array.append(str(i))
-        return empty_array
-fizzbuzz=Solution()
-fizzbuzz.fizzBuzz(3)
+                array.append(str(i))
+        return array
